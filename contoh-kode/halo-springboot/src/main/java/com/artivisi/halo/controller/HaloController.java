cf953cf320f8c5adf2091e7da3174557ba034285
Endy Muhardin <endy.muhardin@gmail.com>
2016-01-12 17:27:54 +0700

    contoh membuat halaman web sederhana dengan Spring Boot

--- contoh-kode/halo-springboot/src/main/java/com/artivisi/halo/controller/HaloController.java
+++ contoh-kode/halo-springboot/src/main/java/com/artivisi/halo/controller/HaloController.java
@@ -1,8 +1,16 @@
 package com.artivisi.halo.controller;
 
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
 public class HaloController{
     
     @RequestMapping("/halo")
-    public void halo(){}
+    public void halo(Model m){
+    		m.addAttribute("waktu", new Date());
+    }
 }
