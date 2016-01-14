020c6dd4bb395b65991565e80de3cf6a4798c382
Endy Muhardin <endy.muhardin@gmail.com>
2016-01-14 19:32:34 +0700

    implement REST API cari by id

--- contoh-kode/halo-springboot/src/main/java/com/artivisi/halo/controller/InstitusiController.java
+++ contoh-kode/halo-springboot/src/main/java/com/artivisi/halo/controller/InstitusiController.java
@@ -3,22 +3,27 @@ package com.artivisi.halo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.artivisi.halo.dao.InstitusiDao;
 import com.artivisi.halo.entity.Institusi;
 
-@Controller
+@RestController
+@RequestMapping("/institusi")
 public class InstitusiController {
 	
 	@Autowired private InstitusiDao institusiDao;
 	
-	@RequestMapping(value="/institusi", method = RequestMethod.GET)
-	@ResponseBody
+	@RequestMapping(value="/", method = RequestMethod.GET)
 	public Page<Institusi> semua(Pageable pageable){
 		return institusiDao.findAll(pageable);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public Institusi cariById(@PathVariable("id") Institusi i){
+		return i;
 	}
 }
--- contoh-kode/halo-springboot/src/test/java/com/artivisi/halo/controller/InstitusiControllerTests.java
+++ contoh-kode/halo-springboot/src/test/java/com/artivisi/halo/controller/InstitusiControllerTests.java
@@ -2,39 +2,54 @@ package com.artivisi.halo.controller;
 
 import java.util.Map;
 
-import javax.transaction.Transactional;
-
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.TestRestTemplate;
 import org.springframework.boot.test.WebIntegrationTest;
-import org.springframework.data.domain.Page;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.RestTemplate;
 
 import com.artivisi.halo.HaloSpringbootApplication;
+import com.artivisi.halo.dao.InstitusiDao;
+import com.artivisi.halo.entity.Institusi;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = HaloSpringbootApplication.class)
-@Transactional
 @Sql(scripts="../dao/insert-sample-institusi.sql")
 @WebIntegrationTest
 public class InstitusiControllerTests {
 	
 	private RestTemplate httpClient = new TestRestTemplate();
-	private String url = "http://localhost:9090/institusi";
+	private String url = "http://localhost:9090/institusi/";
+	
+	@Autowired private InstitusiDao institusiDao;
+	
+	@After
+	public void hapusSampleData(){
+		institusiDao.delete("artivisi");
+	}
 	
 	@Test
-	public void testGetSemuaData() throws InterruptedException{
-		
+	public void testGetSemuaData() {
 		Map<String, Object> hasil = httpClient.getForObject(url, Map.class);
 		Assert.assertNotNull(hasil);
-		System.out.println("Total Elements : "+hasil.get("totalElements"));
+		System.out.println("Total Elements : "+hasil.keySet());
 		Assert.assertEquals(Long.valueOf(1), 
 				Long.valueOf(hasil.get("totalElements").toString()));
 		
+	}
+	
+	@Test
+	public void testGetById(){
+		Institusi i = httpClient.getForObject(url+"artivisi", Institusi.class);
+		Assert.assertNotNull(i);
+		Assert.assertEquals("ArtiVisi Intermedia", i.getNama());
+		
+		Assert.assertNull(httpClient.getForObject(url+"xyz", Institusi.class));
 	}
 }
