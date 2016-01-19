56050676082fd5fd688464e18b67f8ad1fa95b24
Endy Muhardin <endy.muhardin@gmail.com>
2016-01-19 14:44:52 +0700

    implement komponen sederhana

--- contoh-kode/halo-angular2/aplikasi/aplikasi.ts
+++ contoh-kode/halo-angular2/aplikasi/aplikasi.ts
@@ -1,1 +1,13 @@
-export class Aplikasi {}
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'halo-angular',
+    templateUrl: './aplikasi/aplikasi.html'
+})
+export class Aplikasi {
+    tamu: string;
+    klikSimpan(x: string){
+        console.log("Nama : "+x);
+        this.tamu = x;
+    }
+}
