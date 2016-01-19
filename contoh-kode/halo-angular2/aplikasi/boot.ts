9ccfa7ca23b946032b382ed0c60d7c1cfc102d3f
Endy Muhardin <endy.muhardin@gmail.com>
2016-01-19 17:24:14 +0700

    ganti strategi routing menjadi hash-based

--- contoh-kode/halo-angular2/aplikasi/boot.ts
+++ contoh-kode/halo-angular2/aplikasi/boot.ts
@@ -1,7 +1,12 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {ROUTER_PROVIDERS} from 'angular2/router';
+
+import {provide} from 'angular2/core';
+import {LocationStrategy,HashLocationStrategy} from 'angular2/router';
+
 import {Aplikasi} from './aplikasi';
 
 bootstrap(Aplikasi, [
-    ROUTER_PROVIDERS
+    ROUTER_PROVIDERS, 
+    provide(LocationStrategy, {useClass: HashLocationStrategy})
 ]);
