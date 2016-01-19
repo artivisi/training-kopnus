8f7b054fee60e07b128329a922c91d4b1c672c43
Endy Muhardin <endy.muhardin@gmail.com>
2016-01-19 14:45:30 +0700

    implement url userinfo

--- contoh-kode/halo-springboot/src/main/java/com/artivisi/halo/controller/HaloController.java
+++ contoh-kode/halo-springboot/src/main/java/com/artivisi/halo/controller/HaloController.java
@@ -1,10 +1,12 @@
 package com.artivisi.halo.controller;
 
+import java.security.Principal;
 import java.util.Date;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HaloController{
@@ -12,5 +14,11 @@ public class HaloController{
     @RequestMapping("/halo")
     public void halo(Model m){
     		m.addAttribute("waktu", new Date());
+    }
+    
+    @RequestMapping("/saya")
+    @ResponseBody
+    public Principal infoUser(Principal u){
+    		return u;
     }
 }
