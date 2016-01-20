03d057d3a8bb816c6903c1dba2756b121e523ef8
Endy Muhardin <endy.muhardin@gmail.com>
2016-01-20 18:23:35 +0700

    implement API gateway dengan Spring Cloud Zuul

--- contoh-kode/halo-springcloud/src/main/java/com/artivisi/halo/HaloSpringcloudApplication.java
+++ contoh-kode/halo-springcloud/src/main/java/com/artivisi/halo/HaloSpringcloudApplication.java
@@ -2,8 +2,10 @@ package com.artivisi.halo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class HaloSpringcloudApplication {
 
 	public static void main(String[] args) {
