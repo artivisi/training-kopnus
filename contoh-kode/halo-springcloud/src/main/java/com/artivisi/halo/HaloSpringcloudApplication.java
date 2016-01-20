d72a48fd958e553a42c6fffac8daf051828eb0a4
Endy Muhardin <endy.muhardin@gmail.com>
2016-01-20 17:54:43 +0700

    starter project spring

---
+++ contoh-kode/halo-springcloud/src/main/java/com/artivisi/halo/HaloSpringcloudApplication.java
@@ -0,0 +1,12 @@
+package com.artivisi.halo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaloSpringcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaloSpringcloudApplication.class, args);
+	}
+}
